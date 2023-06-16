ea01a85aa1f9cc5a6b13bec91afe8afefad040dc
Tejaswini Hirudkar <121488475+TejaswiniHirudkar@users.noreply.github.com>
2023-06-16 19:49:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function volume_sphere(event) {
   var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
 
   // Display the calculated volume in the volume field
-  document.getElementById('volume').value = volume.toFixed(2);
+  document.getElementById('volume').value = volume.toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
