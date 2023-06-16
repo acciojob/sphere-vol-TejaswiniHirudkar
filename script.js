13d766e8887ca842787f90b2e13e4f64bb9a455a
Tejaswini Hirudkar <121488475+TejaswiniHirudkar@users.noreply.github.com>
2023-06-16 19:43:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function volume_sphere() {
     //Write your code here
-  
+	var radius=document.getElementById('radius').value;
+	var vol=(4/3)* Math.PI * Math.pow(radius, 3);
+	document.getElementById('volume').value=vol.toFixed(2);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
