48fe550a82a670bd09a2497fd3061fe1c5c0ee94
Tejaswini Hirudkar <121488475+TejaswiniHirudkar@users.noreply.github.com>
2023-06-16 19:46:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
-	var radius=document.getElementById('radius').value;
-	var vol=(4/3)* Math.PI * Math.pow(radius, 3);
-	document.getElementById('volume').value=vol.toFixed(2);
+	event.preventDefault(); // Prevent form submission and page reload
+
+  // Get the value of the radius from the input field
+  var radius = parseFloat(document.getElementById('radius').value);
+
+  // Calculate the volume of the sphere using the formula V = (4/3) * π * r^3
+  var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+  // Display the calculated volume in the volume field
+  document.getElementById('volume').value = volume.toFixed(2);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
